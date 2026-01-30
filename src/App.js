afc31d8c1e6f72fa65ee82cc7b79b47fd727e7af
Nishtha Aggarwal <145250198+nishthaaggarwal15@users.noreply.github.com>
2026-01-31 02:48:21 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
+import rough from 'roughjs';
 
 function App() {
+  const canvasRef = useRef();
+  useEffect(()=>{
+const canvas = canvasRef.current; // to select the canvas 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const context = canvas.getContext("2d");
+
+const roughCanvas = rough.canvas(canvas);
+
+const generator = roughCanvas.generator;
+// ...........................x axis, y axis, size 
+let rect1 = generator.rectangle(10, 10, 100, 100);
+let rect2 = generator.rectangle(10, 120, 100, 100, {
+  fill: "red", 
+  stroke: "blue"});
+roughCanvas.draw(rect1);
+roughCanvas.draw(rect2);
+
+context.fillRect(0, 0, 150, 75);
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <canvas  ref = {canvasRef}/>
+      {/* <h1>White board app </h1> */}
     </div>
   );
 }
